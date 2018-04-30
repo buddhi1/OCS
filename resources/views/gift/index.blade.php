53febaf0945c03ebb0c607974f1d80154d07fc10
Emrah <emrahsarboz@gmail.com>
2018-04-30 15:14:35 -0500

    design update

--- resources/views/gift/index.blade.php
+++ resources/views/gift/index.blade.php
@@ -3,70 +3,117 @@
 <head>
 	<title></title>
 </head>
-<body>
-<h1>Gift</h1>
-@if ($errors->any())
-	<div class="alert alert-danger">
-		<ul>
-		@foreach ($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-		</ul>
-	</div><br />
-@endif
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-@if(\Session::has('success'))
-  <div class="alert alert-success">
-      {{\Session::get('success')}}
-  </div>
-@endif
-<div>
-	<form action="{{url('/child/gift')}}" method="POST">
-		{{ csrf_field() }}
-		<input type="text" name="item">
-		<input type="hidden" name="child_id" value="{{$child_id}}">
-		<select name="type">
-			<option value="0">Gift</option>
-			<option value="1">School item</option>
-		</select>
-		<button type="submit">Add</button>
-	</form>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+
+<style media="screen">
+	.btn-danger{
+
+			margin-left: -250px;
+	}
+	.btn-primary{
+		margin-left: -150px;
+	}
+
+	#btnprimary{
+		margin-left: -1px;
+		margin-top:	5px;
+	}
+	.selectClass{
+		margin-top: 10px;
+	}
+
+
+
+	</style>
+<body>
+
+
+<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-xs-12">
+
+				<div>
+					<legend>Gift</legend>
+					<form action="{{url('/child/gift')}}" method="POST">
+						{{ csrf_field() }}
+						<input class="form-control" type="text" name="item">
+						<input class="form-control" type="hidden" name="child_id" value="{{$child_id}}">
+						<select class="selectClass" name="type">
+							<option value="0">Gift</option>
+							<option value="1">School item</option>
+						</select>
+						<br>
+						<button  id = "btnprimary" class="btn btn-primary" type="submit">Add</button>
+					</form>
+				</div>
+
+			</div>
+		</div>
 </div>
-<div>
-	<table border="1">
-		<tr>
-			<th>ID</th>
-			<th>Item</th>
-			<th>Type</th>
-			<th>Edit</th>
-			<th>Delete</th>
-		</tr>
-		@foreach($gifts as $gift)
-		<tr>
-			<td>{{$gift->id}}</td>
-			<td>{{$gift->item}}</td>
-			<td>
-				@if($gift->type == 0)
-					Gift
-				@elseif($gift->type == 1)
-					School Item
-				@endif
-			</td>
-			<td>
-				<form  action="{{url('child/gift', [$gift->id])}}/edit">
-					<button>Edit</button>
-				</form>
-			</td>
-			<td>
-				<form action="{{url('child/gift', [$gift->id])}}" method="POST">
-   					{{method_field('DELETE')}}
-					{{ csrf_field() }}
-					<button>Delete</button>
-				</form>
-			</td>
-		</tr>
-		@endforeach
-	</table>
+
+
+
+
+
+<div class="container">
+
+	<div class="row">
+
+			<div class="col-md-12 col-xs-12">
+
+
+				<div>
+					<table class="table table-bordred table-striped">
+						<tr>
+							<th>ID</th>
+							<th>Item</th>
+							<th>Type</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+						@foreach($gifts as $gift)
+						<tr>
+							<td>{{$gift->id}}</td>
+							<td>{{$gift->item}}</td>
+							<td>
+								@if($gift->type == 0)
+									Gift
+								@elseif($gift->type == 1)
+									School Item
+								@endif
+							</td>
+							<td>
+								<form  action="{{url('child/gift', [$gift->id])}}/edit">
+									<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+								</form>
+							</td>
+							<td>
+								<form action="{{url('child/gift', [$gift->id])}}" method="POST">
+										{{method_field('DELETE')}}
+									{{ csrf_field() }}
+									<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+								</form>
+							</td>
+						</tr>
+						@endforeach
+					</table>
+				</div>
+
+
+
+			</div>
+
+	</div>
+
 </div>
+
+
+
 </body>
 </html>
