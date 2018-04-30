58435639035742243c14986de8919bcfba3a3d1a
Emrah <emrahsarboz@gmail.com>
2018-04-29 19:59:48 -0500

    design update

--- resources/views/agency/edit.blade.php
+++ resources/views/agency/edit.blade.php
@@ -3,6 +3,12 @@
 <head>
 	<title></title>
 </head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <body>
 @if ($errors->any())
 <div class="alert alert-danger">
@@ -22,29 +28,69 @@
 	<form action="{{url('agency', $agency->id)}}" method="POST">
 		{{method_field('PATCH')}}
 		{{csrf_field()}}
-<h1>Case Worker Information</h1>
-<label>first name</label>
-<input type="text" name="first name" value="{{$agency->first_name}}">
 
-<label>last name</label>
-<input type="text" name="last name" value="{{$agency->last_name}}">
 
-<label>phone</label>
-<input type="text" name="phone" value="{{$agency->phone}}">
 
-<label>address</label>
-<input type="text" name="address" value="{{$agency->address}}">
 
-<label>zip code</label>
-<input type="text" name="zip_code" value="{{$agency->zip_code}}">
+<div class="container">
 
-<label>country</label>
-<input type="text" name="country" value="{{$agency->country}}">
 
-<label>e-mail</label>
-<input type="text" name="email" value="{{$agency->email}}">
+          <div class="row">
+                <div class="col-md-12 col-xs-12">
+                  <div class="form-group">
+                    <div class="form-group">
+                      <label for="example-text-input" class="col-2 col-form-label">What is your first name??</label>
+                      <div class="col-12">
+                        <input class="form-control" type="text" value="{{$agency->first_name}}"  id="example-text-input"  name="first name" required >
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="example-search-input" class="col-2 col-form-label">What is your last name?</label>
+                      <div class="col-12">
+                        <input class="form-control" type="text"  value="{{$agency->last_name}}"id="example-search-input" name="last name">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="example-email-input" class="col-2 col-form-label">What is your phone number?</label>
+                      <div class="col-10">
+                        <input class="form-control" type="number" value="{{$agency->phone}}" id="example-email-input" name="phone">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="example-url-input" class="col-2 col-form-label">What is your address?</label>
+                      <div class="col-10">
+                        <input class="form-control" type="tel" value="{{$agency->address}}" id="example-url-input" name="address">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="example-tel-input" class="col-2 col-form-label">What is your zip code?</label>
+                      <div class="col-10">
+                        <input class="form-control" type="tel" value="{{$agency->zip_code}}" id="example-tel-input" name="zip_code">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="example-password-input" class="col-2 col-form-label">Which country</label>
+                      <div class="col-10">
+                        <input class="form-control" type="text" value="{{$agency->country}}" id="example-password-input" name="country">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="example-number-input" class="col-2 col-form-label">What is your email?</label>
+                      <div class="col-10">
+                        <input class="form-control" type="email" value="{{$agency->email}}" id="example-number-input" name="email">
+                      </div>
+                    </div>
 
-<button type="Submit">Update</button>
+										<div class="container">
+          <div class="row">
+            <div class="col-xs-4 col-sm-4 col-md-6">
+          <button class="btn btn-primary btn-sx" type="Submıt">Submit</button>
+        </div>
+          </div>
+        </div>
+
+
+
 </form>
 </body>
 </html>
--- resources/views/agency/index.blade.php
+++ resources/views/agency/index.blade.php
@@ -1,7 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Case Worker</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+
+<style media="screen">
+	.btn-danger{
+
+			margin-left: -90px;
+	}
+	.btn-primary{
+		margin-left: -53px;
+	}
+</style>
+
 </head>
 <body>
 @if(\Session::has('agency'))
@@ -20,46 +43,82 @@
   </div>
   @endif
 
-	<table border="1">
-		<tr>
-			<th>id</th>
-			<th>Name</th>
-			<th>phone</th>
-			<th>address</th>
-			<th>zip_code</th>
-			<th>country</th>
-			<th>email</th>
-			<th>Edit</th>
-			<th>Delete</th>
+<div class="container">
 
-		</tr>
-		@foreach($agencies as $agency)
-		<tr>
-			<td>{{$agency->id}}</td>
-			<td>{{$agency->last_name}} ,{{$agency->first_name}}</td>
-			<td>{{$agency->phone}}</td>
-			<td>{{$agency->address}}</td>
-			<td>{{$agency->zip_code}}</td>
-			<td>{{$agency->country}}</td>
-			<td>{{$agency->email}}</td>
-			
-    					
-			<td>
-				<form  action="{{url('agency', [$agency->id])}}/edit">
-					<button>Edit</button>
-				</form>
-			</td>
-			<td>
-				<form action="{{url('agency', [$agency->id])}}" method="POST">
-   					 {{method_field('DELETE')}}
-					{{ csrf_field() }}
-					<button>Delete</button>
-					<input type="hidden" value="{{$agency->id}}" name="id">
-				</form>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-@endif
+	<div class="row">
+
+		<div class="col-md-12">
+
+			<div class="table-responsive">
+
+				<table class="table table-bordred table-striped">
+					<tr>
+						<th>id</th>
+						<th>Name</th>
+						<th>phone</th>
+						<th>address</th>
+						<th>zip_code</th>
+						<th>country</th>
+						<th>email</th>
+						<th>Edit</th>
+						<th>Delete</th>
+
+					</tr>
+					@foreach($agencies as $agency)
+					<tr>
+						<td>{{$agency->id}}</td>
+						<td>{{$agency->last_name}} ,{{$agency->first_name}}</td>
+						<td>{{$agency->phone}}</td>
+						<td>{{$agency->address}}</td>
+						<td>{{$agency->zip_code}}</td>
+						<td>{{$agency->country}}</td>
+						<td>{{$agency->email}}</td>
+
+
+
+
+
+
+
+
+
+
+
+						<td>
+							<form  action="{{url('agency', [$agency->id])}}/edit">
+								    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+							</form>
+						</td>
+						<td>
+							<form action="{{url('agency', [$agency->id])}}" method="POST">
+									 {{method_field('DELETE')}}
+								{{ csrf_field() }}
+								    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+								<input type="hidden" value="{{$agency->id}}" name="id">
+							</form>
+						</td>
+					</tr>
+					@endforeach
+				</table>
+			@endif
+
+
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+
+
+
+
+
+
+
 </body>
 </html>
