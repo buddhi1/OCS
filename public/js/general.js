de7b10d2420f0f2317a1628b7238d56afb7f9d5d
buddhi1 <buddhiashan8@gmail.com>
2018-04-29 19:13:33 -0500

    child ajax done

--- public/js/general.js
+++ public/js/general.js
@@ -6,7 +6,19 @@ var loadSchoolsByName = function() {
 		obj = JSON.parse(res); 
 		document.getElementById('school-list').innerHTML = '';
 		for (var i = obj.length - 1; i >= 0; i--) {
-			document.getElementById('school-list').innerHTML += '<option value="'+obj[i].id+'">'+obj[i].name+'</option>';
+			document.getElementById('school-list').innerHTML += '<li>'
+																	+obj[i].name
+																	+'<input type="hidden" value="'
+																	+obj[i].id
+																	+'"/></li>';
+		}
+
+		//adding on click event to all the created options
+		var elements = document.querySelectorAll("#school-list");
+		for (var i = 0; i < elements[0].childNodes.length; i++) {
+		  	elements[0].childNodes[i].addEventListener("click", function(e) {
+		    	selectOptionSchool(e);
+			});
 		}
 		
 	});
@@ -20,8 +32,98 @@ var loadCaseworkersByName = function() {
 		obj = JSON.parse(res); 
 		document.getElementById('caseworker-list').innerHTML = '';
 		for (var i = obj.length - 1; i >= 0; i--) {
-			document.getElementById('caseworker-list').innerHTML += '<li>'+obj[i].last_name+', '+obj[i].first_name+'</li>';
+			document.getElementById('caseworker-list').innerHTML += '<li id="cli">'
+																		+obj[i].last_name
+																		+', '
+																		+obj[i].first_name
+																		+'<input type="hidden" value="'
+																		+obj[i].id
+																		+'"/></li>';
+		}
+
+		//adding on click event to all the created options
+		var elements = document.querySelectorAll("#caseworker-list");
+		for (var i = 0; i < elements[0].childNodes.length; i++) {
+		  	elements[0].childNodes[i].addEventListener("click", function(e) {
+		    	selectOption(e);
+			});
 		}
 		
 	});
+}
+
+// load all the advocate with the given name
+var loadAdvocateByName = function() {
+	var name = document.getElementById('advocate_id').value;
+
+	sendRequestToServerGet(url+'/advocate/all', 'name='+name, function(res) {
+		obj = JSON.parse(res); 
+		document.getElementById('advocate-list').innerHTML = '';
+		for (var i = obj.length - 1; i >= 0; i--) {
+			document.getElementById('advocate-list').innerHTML += '<li id="cli">'
+																		+obj[i].last_name
+																		+', '
+																		+obj[i].first_name
+																		+'<input type="hidden" value="'
+																		+obj[i].id
+																		+'"/></li>';
+		}
+
+		//adding on click event to all the created options
+		var elements = document.querySelectorAll("#advocate-list");
+		for (var i = 0; i < elements[0].childNodes.length; i++) {
+		  	elements[0].childNodes[i].addEventListener("click", function(e) {
+		    	selectOptionAdvocate(e);
+			});
+		}
+		
+	});
+}
+
+//dropdown expand
+var caseworkerDropDown = function() {
+    document.getElementById('caseworker-list').style.display = 'block'; 
+	loadCaseworkersByName();	
+}
+
+//dropdown shrink for caeworker
+var caseworkerDropShrink = function() {
+	// document.getElementById('caseworker-list').classList.add('caseworker-list-shrink');
+	console.log(document.activeElement.id);
+	if (document.activeElement.id != 'cli') {
+		document.getElementById('caseworker-list').style.display = 'none'; 
+	}
+}
+
+//option select method for case wrokers
+var selectOption = function(e) {
+	document.getElementById('caseworker_id').value = e.toElement.innerText;
+	document.getElementById('caseworker').value = e.toElement.childNodes[1].value;
+    document.getElementById('caseworker-list').style.display = 'none'; 
+}
+
+//option select method for schools
+var selectOptionSchool = function(e) {
+	document.getElementById('school_name').value = e.toElement.innerText;
+	document.getElementById('school').value = e.toElement.childNodes[1].value;
+    document.getElementById('school-list').style.display = 'none'; 
+}
+
+//option select method for advocate
+var selectOptionAdvocate = function(e) {
+	document.getElementById('advocate_id').value = e.toElement.innerText;
+	document.getElementById('advocate').value = e.toElement.childNodes[1].value;
+    document.getElementById('advocate-list').style.display = 'none'; 
+}
+
+//dropdown expand
+var schoolDropDown = function() {
+    document.getElementById('school-list').style.display = 'block'; 
+	loadSchoolsByName();	
+}
+
+//dropdown expand
+var loadAdvocateByName = function() {
+    document.getElementById('advocate-list').style.display = 'block'; 
+	loadCaseworkersByName();	
 }
--- resources/views/child/create.blade.php
+++ resources/views/child/create.blade.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+ 
+
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"
                integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
                crossorigin="anonymous">
@@ -13,6 +16,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
     <link rel="stylesheet" href="{{url('/')}}/css/ChildrensInformation.css">
   </head>
   <body>
@@ -85,7 +94,7 @@
                     <div class="form-group">
                       <label for="example-url-input" class="col-2 col-form-label">What is the Child's DOB?</label>
                       <div class="col-10">
-                        <input class="form-control" type="text" value="" id="example-url-input" name="dob">
+                        <input class="form-control" id="datepicker" size="30" type="text" name="dob">
                       </div>
                     </div>
                    <!--  <div class="form-group">
@@ -96,15 +105,19 @@
                     </div> -->
                     <div class="form-group">
                       <label for="example-password-input" class="col-2 col-form-label">What is the name of child's caseworker?</label>
-                      <div class="col-10">
-                        <input class="form-control" type="text" value="" name="caseworker_id" id="caseworker_id">
-                        <ul id="caseworker-list"></ul>
+                      <div class="col-10" id="caseworker_div">
+                        <input class="form-control" type="text" value="" name="caseworker" id="caseworker_id">
+                        <input class="form-control" type="hidden" value="" name="caseworker_id" id="caseworker">
+                        <ul class="caseworker-list" id="caseworker-list"></ul>
                       </div>
                     </div>
                     <div class="form-group">
                       <label for="example-number-input" class="col-2 col-form-label">Who is the name of child's CASA advocate?</label>
                       <div class="col-10">
-                        <input class="form-control" type="text" value="" id="example-number-input" name="advocate_id">
+                        <input class="form-control" type="text" value="" name="advocate" id="advocate_id">
+                        <input class="form-control" type="hidden" value="" name="advocate_id" id="advocate">
+                        <ul class="advocate-list" id="advocate-list"></ul>
+
                       </div>
                     </div>
 
@@ -120,9 +133,10 @@
        </div> -->
        <div class='col-sm-4'>
            <div class='form-group'>
-               <label for="user_firstname">What is the name of school?</label>
-               <input class="form-control" id="school_name" name="school_name" size="30" type="text" />
-               <select id="school-list"></select>
+            <label for="user_firstname">What is the name of school?</label>
+            <input class="form-control" id="school_name" name="school_name" size="30" type="text" />
+            <input class="form-control" type="hidden" value="" name="school_id" id="school">
+            <ul class="caseworker-list" id="school-list"></ul>
            </div>
        </div>
        <div class='col-sm-4'>
@@ -180,12 +194,31 @@
     <script type="text/javascript" src="{{url('/')}}/js/general.js"></script>
     <script type="text/javascript">
 
-      document.getElementById("caseworker_id").addEventListener("keyup", loadCaseworkersByName);
-      document.getElementById("school_name").addEventListener("keyup", loadSchoolsByName);
       window.onload = function() {
         loadSchoolsByName();
         loadCaseworkersByName();
+        document.getElementById('caseworker-list').style.display = 'none'; 
+        document.getElementById('school-list').style.display = 'none'; 
       };
+      document.getElementById("caseworker_id").addEventListener("focus", caseworkerDropDown);
+      document.getElementById("caseworker_id").addEventListener("keyup", caseworkerDropDown);
+      // document.getElementById("caseworker_id").addEventListener("focusout", caseworkerDropShrink);
+      document.getElementById("school_name").addEventListener("focus", schoolDropDown);      
+      document.getElementById("school_name").addEventListener("keyup", schoolDropDown);
+      document.getElementById("advocate_id").addEventListener("focus", loadAdvocateByName);      
+      document.getElementById("advocate_id").addEventListener("keyup", loadAdvocateByName);
+
+      //datepicker
+      $( function() {
+        $( "#datepicker" ).datepicker({
+          changeMonth: true,
+          changeYear: true
+        });
+        $( "#anim" ).on( "change", function() {
+          $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+        });
+      } );
+      
     </script>
   </body>
 </html>
